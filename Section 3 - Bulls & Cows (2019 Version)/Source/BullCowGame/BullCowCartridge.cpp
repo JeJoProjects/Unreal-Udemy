222ba637b060e8d8b10e13e5406e4de0722cb28a
JeJo <email2jejo@gmail.com>
2020-03-22 11:42:31 +0100

    Section 3: Bulls & Cows (2019 Version) -  Coding The Basic Game Loop

--- Section 3 - Bulls & Cows (2019 Version)/Source/BullCowGame/BullCowCartridge.cpp
+++ Section 3 - Bulls & Cows (2019 Version)/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,22 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bull Cows Game!"));
+    PrintLine(TEXT("Press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
+    // PrintLine(Input);
+    const FString HiddenWorld{TEXT("let")};
 
+    if(HiddenWorld == Input)
+    {
+        PrintLine(TEXT("!!! Congratz !!!\n You won!..."));
+    }
+    else
+    {
+        PrintLine(TEXT("You lost!..."));
+    }
 }
