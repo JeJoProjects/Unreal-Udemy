fe43103ebcf8ea7d15ab903bace636f6b5219939
JeJo <email2jejo@gmail.com>
2020-03-25 15:21:19 +0100

    Section 3: Bulls & Cows (2019 Version) - Logical Operators and more

--- Section 3 - Bulls & Cows (2019 Version)/Source/BullCowGame/BullCowCartridge.cpp
+++ Section 3 - Bulls & Cows (2019 Version)/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,25 +1,109 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+#include "HiddenWordsList.h"
+
+#include <map>
+#include <cstddef>     // std::size_t
+#include <type_traits> // std::decay_t
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(TEXT("Welcome to Bull Cows Game!"));
-    PrintLine(TEXT("Press enter to continue..."));
+
+    // setting up the Game
+    SetupGame();
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
-    ClearScreen();
-    // PrintLine(Input);
-    const FString HiddenWorld{TEXT("let")};
 
-    if(HiddenWorld == Input)
+    if (bGameOver)
     {
-        PrintLine(TEXT("!!! Congratz !!!\n You won!..."));
+        ClearScreen();
+        SetupGame();
     }
-    else
+    else // else checking the player guess.
     {
-        PrintLine(TEXT("You lost!..."));
+        ProcessGuess(Input);
     }
+}
+
+void UBullCowCartridge::SetupGame() noexcept
+{
+    // welcoming the player
+    PrintLine(TEXT("Welcome to Bull Cows Game!"));
+
+    HiddenWorld = TEXT("let");
+    Lives = 4;
+    bGameOver = false;
+
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWorld.Len());
+    PrintLine(TEXT("You have %i lives."), Lives);
+    PrintLine(TEXT("Type in your guess and \npress enter to continue..."));
+}
+
+bool UBullCowCartridge::IsIsogram(const FString &UserGuess) const noexcept
+{
+    using ElementType = std::decay_t<decltype(*UserGuess.begin())>;
+    std::map<ElementType, std::size_t> CharMap;
+    for(const ElementType element: UserGuess)
+    {
+        CharMap[element]++;
+        if(CharMap[element] > 1u)
+            return false;
+    }
+    return true;
+}
+
+void UBullCowCartridge::LostMessage() noexcept
+{
+    PrintLine(TEXT("You lost a life!"));
+    PrintLine(TEXT("You have %i lives remaining"), --Lives);
+    PrintLine(TEXT("Try guess again..."));
+}
+
+void UBullCowCartridge::ProcessGuess(const FString &UserGuess) noexcept
+{
+    // matching case
+    if (UserGuess == HiddenWorld)
+    {
+        ClearScreen();
+        PrintLine(TEXT("!!! Congrats !!!\n You have won!..."));
+        EndGame();
+        return; // done here!
+    }
+
+    // checking the length
+    if (UserGuess.Len() != HiddenWorld.Len())
+    {
+        ClearScreen();
+        PrintLine(TEXT("The hidden word is %i charectors long."), HiddenWorld.Len());
+        LostMessage();
+        return; // done here!
+    }
+
+    // check if Isogram
+    if (!IsIsogram(UserGuess))
+    {
+        ClearScreen();
+        PrintLine(TEXT("No repeating letters, guess again!"));
+        LostMessage();
+        return; // done here!
+    }
+
+    // decrement the lives and validate
+    if (--Lives <= 0)
+    {
+        ClearScreen();
+        PrintLine(TEXT("You have no lives left!"));
+        PrintLine(TEXT("The hidden word was: %s"), *HiddenWorld);
+        EndGame();
+        return; // done here!
+    }
+}
+
+void UBullCowCartridge::EndGame() noexcept
+{
+    bGameOver = true;
+    PrintLine(TEXT("\nPress enter to play again!..."));
 }
--- Section 3 - Bulls & Cows (2019 Version)/Source/BullCowGame/BullCowCartridge.h
+++ Section 3 - Bulls & Cows (2019 Version)/Source/BullCowGame/BullCowCartridge.h
@@ -6,16 +6,24 @@
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 {
 	GENERATED_BODY()
 
-	public:
-	virtual void BeginPlay() override;
-	virtual void OnInput(const FString& Input) override;
+public:
+	void BeginPlay() override;
+	void OnInput(const FString &) override;
 
-	// Your declarations go below!
-	private:
-	
+private:
+	void SetupGame() noexcept;
+	bool IsIsogram(const FString &) const noexcept;
+	void LostMessage() noexcept;
+	void ProcessGuess(const FString &) noexcept;
+	void EndGame() noexcept;
+
+private:
+	FString HiddenWorld;
+	int32 Lives;
+	bool bGameOver;
 };
